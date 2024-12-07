a15a2b8d8871cdc179c1c8e01c87b5051a3f8c49
sethumadhavan7 <chandrabosechandrabose9@gmail.com>
2024-12-07 11:05:10 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,70 +7,37 @@ import Helper from './Pages/Helper';
 import Chat from './Pages/Chat';
 import OpenChat from './Components/OpenChat';
 import { useEffect, useRef, useState } from 'react';
-import { io } from 'socket.io-client';
+import {io} from 'socket.io-client'
 import CallPage from './Pages/CallPage';
 import HelperRequest from './Pages/HelperRequest';
 import Cryptopages from './Pages/Cryptopages';
 
 function App() {
-  const [User, setUser] = useState(undefined);
-  const [Chats, setChat] = useState(undefined);
+  const [User,setUser] = useState(undefined);
+  const [Chats,setChat] = useState(undefined);
   const socket = useRef();
 
-  // Establish WebSocket connection
-  useEffect(() => {
-    if (User) {
-      socket.current = io(process.env.REACT_APP_SOCKET_URL || "http://localhost:3500");
-      socket.current.emit("add-user", User._id);
-
-      // WebSocket error handling
-      socket.current.on("connect_error", (err) => {
-        console.error("Socket connection error:", err.message);
-      });
+  useEffect(()=>{
+    if(User){
+      socket.current = io("http://localhost:3500")
+      socket.current.emit("add-user",User._id)
     }
-  }, [User]);
-
-  // Private route component for authenticated users
-  const PrivateRoute = ({ children }) => {
-    return User ? children : <Navigate to="/login" />;
-  };
-
+  },[User])
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/client" element={<Client setUser={setUser} setChat={setChat} />} />
-        <Route path="/helper" element={<Helper />} />
-        <Route path="/request" element={<HelperRequest setUser={setUser} />} />
-        <Route
-          path="/chat-page"
-          element={
-            <PrivateRoute>
-              <Chat />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/crypto" element={<Cryptopages />} />
-        <Route
-          path="/test"
-          element={
-            <PrivateRoute>
-              <CallPage Chats={Chats} />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/priv-chat"
-          element={
-            <PrivateRoute>
-              <OpenChat Chats={Chats} User={User} socket={socket} />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </>
+      <>
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/client" element={<Client setUser = {setUser} setChat = {setChat} />}/>
+          <Route path="/helper" element={<Helper/>}/>
+          <Route path="/request" element={<HelperRequest setUser = {setUser}/>}/>
+          <Route path = "/chat-page" element={<Chat/>} />
+          <Route path = "/crypto" element={<Cryptopages/>} />
+          <Route path = "/test" element={<CallPage Chats = {Chats} />} />
+          <Route path="/priv-chat" element={<OpenChat Chats={Chats} User={User} socket = {socket} />} />
+        </Routes>
+      </>
   );
 }
 
