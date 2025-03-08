4e6622dd0af5cf60640b263ff26bced5a89276a8
sethumadhavan7 <chandrabosechandrabose9@gmail.com>
2025-03-08 14:15:14 +0530

    Update Menu2.jsx

--- client/src/Components/Menu2.jsx
+++ client/src/Components/Menu2.jsx
@@ -4,44 +4,41 @@ import styled from 'styled-components';
 
 const Menu2 = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const navigate = useNavigate();
+
+  const navigate = useNavigate()
+
 
   const handleToggleMenu = () => {
     setShowMenu(!showMenu);
   };
 
-  const handleLogout = () => {
+  const handleLogout =()=>{
     localStorage.removeItem('Mental-App');
-    navigate('/');
-  };
+    navigate('/')
+  }
 
   return (
     <Div>
       <ToggleMenuButton onClick={handleToggleMenu}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-menu">
-          <line x1="3" y1="12" x2="21" y2="12"></line>
-          <line x1="3" y1="6" x2="21" y2="6"></line>
-          <line x1="3" y1="18" x2="21" y2="18"></line>
-        </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
       </ToggleMenuButton>
       {showMenu && (
         <MenuContainer>
           <Link to={'/helper'}><MenuItem>Home</MenuItem></Link>
           <Link to={'/request'}><MenuItem>Requests</MenuItem></Link>
           <Link to={'/crypto'}><MenuItem>Crypto</MenuItem></Link>
-          <p style={{ color: 'red', textAlign: 'center', cursor: 'pointer' }} onClick={handleLogout}>LogOut</p>
+          <p style={{color:'red',textAlign:'center',cursor:'pointer'}} onClick={handleLogout} >LogOut</p>
         </MenuContainer>
       )}
     </Div>
   );
 };
-
 const Div = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
   z-index: 99;
-`;
+`
 
 const ToggleMenuButton = styled.button`
   background-color: #2e7d32;
@@ -53,7 +50,6 @@ const ToggleMenuButton = styled.button`
   transition: all 0.3s ease-in-out;
   display: flex;
   justify-content: right;
-
   &:hover {
     background-color: #1b5e20;
   }
@@ -66,8 +62,7 @@ const MenuContainer = styled.div`
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   width: 300px;
   z-index: 99;
-
-  a {
+  a{
     text-decoration: none;
     color: inherit;
   }
@@ -79,8 +74,7 @@ const MenuItem = styled.div`
   margin: 2px;
   background-color: #ddf1ce;
   text-align: center;
-
-  &:hover {
+  &:hover{
     background-color: #d1edbb;
   }
 
